f81cfdfc16f191ad9c5ab27c29e65ca3c79d19f5
Jesse Born <jesse.born@gmx.ch>
2024-06-05 15:19:10 +0200

    atempt at gpu processing tiles

--- source/tiledownloader/convert.sh
+++ source/tiledownloader/convert.sh
@@ -2,6 +2,10 @@ find dem_tiled -name '*.tif' > files.txt && gdalbuildvrt swissalps.vrt -input_fi
 
 gdalwarp -overwrite -t_srs EPSG:3857 -dstnodata 0.0 -r bilinear -of GTiff /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/swissalps.vrt /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/cantons/swissalps_reproj.tif
 
+gdalwarp -overwrite -t_srs EPSG:3857 -dstnodata 0.0 -r bilinear -of GTiff /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/swissalps.vrt /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/cantons/swissalps_reproj.tif -co TILED=YES -co BLOCKXSIZE=2048 -co BLOCKYSIZE=2048
+
+
+
 rio rgbify -b -10000 -i 0.10 cantons/swissalps.tif cantons/swissalps_rgb.tif
 gdal2tiles --zoom=9-15 --processes=8 --resampling=near cantons/swissalps_rgb.tif ./tiles_swissalps
 mb-util --image_format=png --scheme=tms ./tiles_swissalps/ ./tilesets/swissalps.mbtiles
