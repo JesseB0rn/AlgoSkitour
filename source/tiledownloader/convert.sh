71fe3991a0e0e14117f9ac46572e75ad724cef80
Jesse Born <jesse.born@gmx.ch>
2024-06-12 14:16:09 +0200

    code to convert to slope

--- source/tiledownloader/convert.sh
+++ source/tiledownloader/convert.sh
@@ -9,3 +9,7 @@ gdalwarp -overwrite -t_srs EPSG:3857 -dstnodata 0.0 -r bilinear -of GTiff /Users
 rio rgbify -b -10000 -i 0.10 cantons/swissalps.tif cantons/swissalps_rgb.tif
 gdal2tiles --zoom=9-15 --processes=8 --resampling=near cantons/swissalps_rgb.tif ./tiles_swissalps
 mb-util --image_format=png --scheme=tms ./tiles_swissalps/ ./tilesets/swissalps.mbtiles
+
+
+
+gdaldem slope /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/swissalps.vrt /Users/jesseb0rn/Documents/repos/Maturaarbeit-AlgoSkitour/source/tiledownloader/slope.tif -of GTiff -b 1 -s 1.0
