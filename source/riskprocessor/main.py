fba39ba0fa055502b20795902948a99ff55fd8f1
Jesse Born <jesse.born@gmx.ch>
2024-06-27 16:43:22 +0200

    better riskmap

--- source/riskprocessor/main.py
+++ source/riskprocessor/main.py
@@ -3,9 +3,12 @@
 import riomucho
 import numpy as np
 
+def riskfn(slope):
+    return np.sqrt(1 / (1 + np.exp(-(slope-28.0) / 3.0))) + np.power((1/45)*slope, 2)
+
 def calculate_grm_risk(slope):
-    expterm = np.sqrt(1 / (1 + np.exp(-(slope-28.0) / 3.0)))
-    return np.maximum(expterm, expterm + ((slope/300) - 0.15))
+    # return np.maximum(expterm, expterm + ((slope/300) - 0.15))
+    return riskfn(slope) / riskfn(90.0)
 
 def read_function(data, window, ij, g_args):
     """Takes an array, and sets any value above the mean to the max, the rest to 0"""
