aebc570b827e85d657ae32c78d7ba98aa6a6efcd
Jesse Born <jesse.born@gmx.ch>
2024-08-19 12:42:04 +0200

    add llb, reflection and attachment

--- source/riskprocessor/main.py
+++ source/riskprocessor/main.py
@@ -1,17 +1,13 @@
-"""Simple reading pattern example"""
-
 import riomucho
 import numpy as np
 
 def riskfn(slope):
-    return np.sqrt(1 / (1 + np.exp(-(slope-28.0) / 3.0))) # + np.power((1/45)*slope, 2)
+    return np.sqrt(1 / (1 + np.exp(-(slope-28.0) / 3.0)))
 
 def calculate_grm_risk(slope):
-    # return np.maximum(expterm, expterm + ((slope/300) - 0.15))
     return riskfn(slope) / riskfn(90.0)
 
 def read_function(data, window, ij, g_args):
-    """Takes an array, and sets any value above the mean to the max, the rest to 0"""
     output = calculate_grm_risk(data[0])
     return output
 
